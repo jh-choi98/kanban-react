603e0d17b4f6266323dd00e07246abc81bc52c3b
Juho Choi <juho.c98@gmail.com>
2024-09-11 19:33:42 -0400

    Updates

--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-import { IToDoState } from "./atoms";
-
-// store in local storage
-export function saveLocalStorage(WholeBoards: IToDoState) {
-  localStorage.setItem("Boards", JSON.stringify(WholeBoards));
-}
